c5f6b24cced678f3d5ef3d45d0e8f1af5be9a213
Sukhleen <sukhleenk1113@gmail.com>
2026-03-25 20:25:59 -0600

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="api-latency-monitor",
-    version="0.1.0",
+    version="0.1.1",
     description="CLI tool to monitor API endpoint latency and detect degradation",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
